5e8d3cf9aafb23de32b5508218670cb591fd4f72
Lualt <officieel.lualt@gmail.com>
2024-06-19 16:26:40 +0200

    extra favicon

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for
-import atexit
+from flask import Flask, render_template, send_from_directory
+import atexit, os
 
 app = Flask(__name__)
 app.jinja_env.add_extension("pypugjs.ext.jinja.PyPugJSExtension")
@@ -19,6 +19,11 @@ def save_visits():
 
   with open("visits.txt", "w") as file:
     file.write(str(VISITS))
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "static/images"),
+      "cherry-blossom_1f338.ico", mimetype="image/vnd.microsoft.icon")
 
 @app.route("/")
 def index():
