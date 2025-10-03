b124f36ca9b632c3001d6615aefa2ba442568c6d
Lualt <git@lua.lt>
2025-10-03 14:29:58 +0200

    implemented http server

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log/slog"
+	"net/http"
+	"strconv"
+	"unicode/utf8"
+)
+
+type PageVariables struct {
+	Views int
+}
+
+func (pageVariables PageVariables) ViewsAsArray() []string {
+	var text string = strconv.Itoa(pageVariables.Views)
+	characters := make([]string, utf8.RuneCountInString(text))
+
+	for i, char := range text {
+		characters[i] = string(char)
+	}
+
+	return characters
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	slog.Info("starting http server on :8080")
+
+	http.HandleFunc("/", index)
+
+	err := http.ListenAndServe(":8080", nil)
+	slog.Error(err.Error())
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	pageVariables := PageVariables{
+		Views: 3444,
+	}
+
+	t, err := template.ParseFiles("./web/templates/index.html")
+	if err != nil {
+		http.Error(w, "Internal server error!", http.StatusInternalServerError)
+		slog.Error("index page error", "error", err.Error())
+		return
+	}
+
+	err = t.Execute(w, pageVariables)
+	if err != nil {
+		http.Error(w, "Internal server error!", http.StatusInternalServerError)
+		slog.Error("index page error", "error", err.Error())
+		return
+	}
+
+	slog.Info("index page loaded")
 }
